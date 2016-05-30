5a1ec1b774284c5e97545670c1f2679bd70b7ea3
ShizukaHara <xsweet926x@gmail.com>
2016-05-30 18:09:09 +0900

    setting to login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525091434) do
+ActiveRecord::Schema.define(version: 20160530082140) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160525091434) do
     t.string   "avater",                 limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
