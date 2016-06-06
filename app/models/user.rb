320511a923c70516609516ac2db61138320e6216
ShizukaHara <xsweet926x@gmail.com>
2016-06-06 12:44:22 +0900

    add avatar setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, ImageUploader
+
   has_attached_file :avatar,
                       styles:  { medium: "300x300#", thumb: "100x100#" }
-  validates_attachment_content_type :avatar,
-                                      content_type: ["image/jpg","image/jpeg","image/png"]
+
 end
