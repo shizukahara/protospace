13dcfd3acd0f28438c1c554985be78ef9f68bc3f
ShizukaHara <xsweet926x@gmail.com>
2016-06-09 15:20:43 +0900

    setting validation more

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :password, length: {minimum: 6}, on: :create
+  validates :password, length: {minimum: 6}, on: :update, allow_blank: true
+
   mount_uploader :avatar, ImageUploader
 
 
