75c2d2ff90839607c497ef6d6553baa98292b2cc
ShizukaHara <xsweet926x@gmail.com>
2016-06-09 15:53:45 +0900

    edit hash space and hash rocket

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :password, length: {minimum: 6}, on: :create
-  validates :password, length: {minimum: 6}, on: :update, allow_blank: true
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :password, length: { minimum: 6 }, on: :update, allow_blank: true
 
   mount_uploader :avatar, ImageUploader
 
