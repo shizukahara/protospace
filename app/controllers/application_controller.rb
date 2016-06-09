09119f7a7eccd909c2c4b12c3564190dbb25e543
ShizukaHara <xsweet926x@gmail.com>
2016-06-09 15:19:29 +0900

    add_frash_types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
+  add_flash_types :success, :info, :warning, :danger
 
   def after_sign_out_path_for(resource)
     '/users/sign_in' # サインアウト後のリダイレクト先URL
