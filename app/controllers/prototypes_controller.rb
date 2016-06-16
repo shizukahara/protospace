e47b1d7cfa509928a8592ef75408f013894607f2
ShizukaHara <xsweet926x@gmail.com>
2016-06-16 17:54:01 +0900

    create action (new, create, index)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,15 +2,35 @@ class PrototypesController < ApplicationController
   before_action :move_to_index, except: :index
   
   def index
-
+    @prototypes = Prototype.all.order('id ASC')
   end
 
- def new
-   
- end
+  def new
+   @prototype = Prototype.new
+   3.times { @prototype.thumbnails.build }
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to action: :index
+  end
+
 
   private
   def move_to_index
      redirect_to action: :index unless user_signed_in?
   end
+
+  def prototype_params
+    params.require(:prototype).permit(
+      :title,
+      :catchcopy,
+      :concept,
+      :user_id,
+      :image_url,
+      thumbnails_attributes: [:id, :status]
+      ).merge(user_id: current_user.id)
+  end
+
+
 end
