2bd9a83859b947e43572ee0cbcd779f4867e7eed
ShizukaHara <xsweet926x@gmail.com>
2016-06-19 14:43:53 +0900

    nothing

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -2,6 +2,6 @@ class Thumbnail < ActiveRecord::Base
   belongs_to :prototype
   mount_uploader :image_url, ImageUploader
 
-  enum sratus: { main: 0, sub: 1 }
+  enum status: { main: 0, sub: 1 }
   validates :status, presence: true
 end
