f957b8ed29fa08fcdd5e3b74a49445eaf5609698
ShizukaHara <xsweet926x@gmail.com>
2016-06-25 15:25:56 +0900

    CRF invalid

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :thumbnails
-  accepts_nested_attributes_for :thumbnails
+  accepts_nested_attributes_for :thumbnails, allow_destroy: true
 
   validates :catchcopy,  presence: true
   validates :concept,     presence: true
