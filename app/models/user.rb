b5fdf413ecf2627538432134dd335159b72f5d39
ShizukaHara <xsweet926x@gmail.com>
2016-07-07 15:17:01 +0900

    solve the validation error because title column unused

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6 }, on: :create
   validates :password, length: { minimum: 6 }, on: :update, allow_blank: true
-  validates :work, :nickname, :title, :profile, :member, presence: true
+  validates :work, :nickname, :profile, :member, presence: true
 
   mount_uploader :avatar, ImageUploader
 
