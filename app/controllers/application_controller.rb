d2f17e2bc956852c1e47f01053691e62cec2a714
ShizukaHara <xsweet926x@gmail.com>
2016-08-03 19:07:13 +0900

    sanitizer become private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,12 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :info, :warning, :danger
 
+
   def after_sign_out_path_for(resource)
     '/users/sign_in' # サインアウト後のリダイレクト先URL
   end
 
+  private
   def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up).push(:profile, :member, :topmedia, :nickname, :avatar, :work)
       devise_parameter_sanitizer.for(:account_update).push(:profile, :member, :topmedia, :nickname, :avatar, :work)
