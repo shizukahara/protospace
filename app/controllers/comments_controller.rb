c2b5a66b42fe39bb6b7023c7e243db13926365fc
ShizukaHara <xsweet926x@gmail.com>
2016-08-05 10:04:11 +0900

    add includes method to solve  N+1 problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,13 @@ class CommentsController < ApplicationController
   before_action :set_prototype
   def create
     @comment = @prototype.comments.create(comment_params)
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comments.includes(:user, :prototype)
      #コメントと結びつくツイートの詳細画面に遷移する
   end
 
   private
   def set_prototype
-    @prototype = Prototype.find(params[:prototype_id])
+    @prototype = Prototype.includes(:user, :thumbnails).find(params[:prototype_id])
   end
 
   def comment_params
