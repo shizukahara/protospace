6cf66e8576c1d030c93fe3592aa09f6a74791fc2
ShizukaHara <xsweet926x@gmail.com>
2016-10-07 14:08:39 +0900

    delete before action "set_prototype" and add  @prototype to create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,12 @@
 class CommentsController < ApplicationController
-  before_action :set_prototype
+
   def create
+    @prototype = Prototype.includes(:user, :thumbnails).find(params[:prototype_id])
     @comment = current_user.comments.create(comment_params)
     @comments = @prototype.comments.includes(:user, :prototype)
   end
 
   private
-  def set_prototype
-    @prototype = Prototype.includes(:user, :thumbnails).find(params[:prototype_id])
-  end
-
   def comment_params
     params.require(:comment).permit(:text).merge(prototype_id: params[:prototype_id])
   end
