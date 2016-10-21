25607b023a92c5d6ee393aef75b8c3388210a4e7
ShizukaHara <xsweet926x@gmail.com>
2016-10-21 23:09:41 +0900

    association between like model and prototype model, add like_user method

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,9 +2,14 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :thumbnails
   has_many :comments
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :thumbnails, allow_destroy: true
 
   validates :catchcopy,  presence: true
   validates :concept,     presence: true
   validates :title,       presence: true
+
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
 end
