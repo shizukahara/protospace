6d8c8868bddb4bafca1daf8b0ac489d4d0b504ce
ShizukaHara <xsweet926x@gmail.com>
2016-10-26 23:09:58 +0900

    delete permit params likes_count and created_at

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -57,8 +57,6 @@ class PrototypesController < ApplicationController
       :concept,
       :user_id,
       :image_url,
-      :likes_count,
-      :created_at,
       thumbnails_attributes: [:id, :status, :image_url]
     )
   end
