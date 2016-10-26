52e0c084c2983cfebd377e10d555b5e1d892a4da
ShizukaHara <xsweet926x@gmail.com>
2016-10-26 23:10:51 +0900

    edit routing indent and delete resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
   root 'prototypes#index'
 
   #ネスト
   resources :prototypes, except: :index do
-     resources :comments, only: :create
-     resources :likes, only: [:create, :destroy]
+    resources :comments, only: :create
+    resources :likes, only: [:create, :destroy]
   end
 
   #controller間のグルーピング
-  scope module: 'prototypes' do
-  resources :newest, :popular
+  scope module: :prototypes do
+    resources :newest, only: :index
+    resources :popular, only: :index
   end
 
   resources :users, only: [:show, :edit, :update]
