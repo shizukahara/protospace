79435f64e4fde991ac90228559ef922d9a04cff5
ShizukaHara <xsweet926x@gmail.com>
2016-11-17 15:01:46 +0900

    delete strong_parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,19 +18,10 @@ class UsersController < ApplicationController
 
   end
 
-
+  # Use callbacks to share common setup or constraints between actions.
+  # Never trust parameters from the scary internet, only allow the white list through.
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_avatar
-    @user = User.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  #def avatar_params
-   # params.require(:user).permit(:description, :avatar)
-  #end
-
   def update_params
     params.require(:user).permit(:image, :text, :avatar, :work, :private, :profile, :member)
   end
