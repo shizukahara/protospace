e25665ab6fa9261d21820b97f6ecb9af0585b6a4
ShizukaHara <xsweet926x@gmail.com>
2017-02-18 11:18:27 +0900

    edit prototype_preview.js

--- app/assets/javascripts/prototype_preview.js
+++ app/assets/javascripts/prototype_preview.js
@@ -1,24 +1,26 @@
-$(window).on("click", function(){
-  $(".js-image-preview").each(function() {
-    var prev = $(this)
-    image = prev.find('input[type=file]');
-    image.change(function(){
-      var file = this.files[0]
-      if (!this.files.length){
-        return;
-      }
-      if (!file.type.match('image.*')) {
-        window.alert('This file is not available!');
-        return;
-      }
-      fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-      fileReader.onload = function(){
-        prev.css({
-          'background-image': 'url(' + fileReader.result + ')',
-          'background-size' : 'cover'
+$(function() {
+  $(".cover-image-upload,.image-upload").on("click", function(){
+      image_id = this.id;
+      var prev = $(this);
+      image = prev.find('input[type=file]');
+      image.change(function(){
+        var file = this.files[0]
+        if (!this.files.length){
+          return;
+        }
+        if (!file.type.match('image.*')) {
+          window.alert('This file is not available!');
+          return;
+        }
+        fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = function(){
+          prev.css({
+            'background-image': 'url(' + fileReader.result + ')',
+            'background-size' : 'cover'
+          });
+          $(`#${image_id}_image`).hide();
+        };
         });
-      };
       });
-    });
-  });
+});
