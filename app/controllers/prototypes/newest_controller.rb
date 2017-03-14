a9bd760ae7fb7e1bf34ce99170c9899fe073b95b
ShizukaHara <xsweet926x@gmail.com>
2017-03-14 16:41:44 +0900

    add pagination per 8

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,6 +1,6 @@
 class Prototypes::NewestController < ApplicationController
   def index
-    @prototypes = Prototype.includes(:user, :thumbnails, :tags).order('created_at DESC')
+    @prototypes = Prototype.includes(:user, :thumbnails, :tags).order('created_at DESC').page(params[:page]).per(8)
     render "prototypes/index"
   end
 end
--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,6 +1,6 @@
 class Prototypes::PopularController < ApplicationController
   def index
-    @prototypes = Prototype.includes(:user, :thumbnails, :tags).order('likes_count DESC')
+    @prototypes = Prototype.includes(:user, :thumbnails, :tags).order('likes_count DESC').page(params[:page]).per(8)
     render "prototypes/index"
   end
 end
