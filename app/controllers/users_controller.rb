340c87ae6e10f8fb6cb4a4efcb7ad4bb287ffc6e
ShizukaHara <xsweet926x@gmail.com>
2017-04-22 19:51:40 +0900

    comment out some method for Rspec test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
+#テストのbefore_action
+
+
   def show
     @user = User.find(params[:id])
-    @prototypes = current_user.prototypes.order("created_at DESC").includes(:tags).page(params[:page]).per(8)
+    #@prototypes = current_user.prototypes.order("created_at DESC").includes(:tags).page(params[:page]).per(8)
+    @prototypes = @user.prototypes
   end
 
   def edit
@@ -10,8 +14,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(update_params)
+    #current_user.update(update_params)
+    @user.update(update_params)
     redirect_to action: :show
+    #テスト用
   end
 
   def destroy
@@ -24,6 +30,10 @@ class UsersController < ApplicationController
   private
   def update_params
     params.require(:user).permit(:image, :text, :avatar, :work, :private, :profile, :member)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
   end
 
 end
