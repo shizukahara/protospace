0089f24369fa0a5941e22fe115a818f24eefc0bc
ShizukaHara <xsweet926x@gmail.com>
2017-04-22 19:53:54 +0900

    create prototypes_controller file for test

---
+++ spec/controllers/prototypes_controller_spec.rb
@@ -0,0 +1,144 @@
+require 'rails_helper'
+
+describe PrototypesController, type: :controller do
+
+    let!(:prototype) { create(:prototype) }
+    let!(:params) {{
+      id: prototype.id,
+      prototype: attributes_for(:prototype, name: 'hoge')
+    }}
+    let!(:invalid_params) {{
+      id: prototype.id,
+      prototype: attributes_for(:prototype, name: nil)
+    }}
+
+    before do
+      @user = User.first
+      allow(controller)
+        .to receive(:current_user)
+        .and_return(@user)
+    end
+    #current_userのスタブを作る。before_action require_loginを通過できる。Qita参照。
+
+
+    describe 'GET #index' do
+      it 'assigns the requested prototypes to @prototypes' do
+        binding.pry
+        prototypes = create_list(:prototype, 8)
+        get :index
+        expect(assigns(:prototypes)).to eq (prototypes.sort{ |a,b| b.created_at <=> a.created_at })
+      end
+
+    #@prototypes = Prototype.includes(:user, :thumbnails, :tags).order('created_at DESC').page(params[:page]).per(8)
+    #indexアクションエラー未解決
+    #そしてMySQLにログインできない悲劇。
+      it "renders the :index template" do
+        prototype = create(:prototype)
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+
+
+
+    describe 'GET #new' do
+      it "renders the :new template" do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+
+    describe 'GET #show' do
+      before do
+        get :show, id: prototype.id
+      end
+
+      it 'assigns the requested prototype to @prototype' do
+        expect(assigns(:prototype)).to eq prototype
+      end
+
+      it 'assigns the requested comment to @comment' do
+        expect(assigns(:comment)).to be_a_new(Comment)
+      end
+
+      it 'assigns likes associated with prototype to @likes' do
+        expect(assigns(:likes)).to eq prototype.likes_count
+      end
+
+      it 'renders the :show template' do
+        expect(response).to render_template :show
+      end
+    end
+
+  #def show
+   # @thumbnails = @prototype.thumbnails.sub
+   # @user = @prototype.user
+   # @comment = Comment.new(prototype_id: @prototype_id)
+   #@comments = @prototype.comments.includes(:user)
+    #@like  = @prototype.likes.find_by(user_id: current_user.id)
+  #end
+
+
+    describe 'GET #edit' do
+      before do
+        get :edit, id: prototype.id
+      end
+
+      it 'assigns the requested prototype to @prototype' do
+        expect(assigns(:prototype)).to eq prototype
+      end
+
+      it 'assigns main_image to @main_content' do
+        expect(assigns(:main_image)).to eq prototype.thumbnails.main
+      end
+
+      it 'assigns sub_images to @sub_contents' do
+        expect(assigns(:sub_images)).to eq prototype.thumbnails.sub
+      end
+
+      it 'renders the :edit template' do
+        expect(response).to render_template :edit
+      end
+    end
+
+
+end
+
+
+
+
+
+
+
+=begin
+メモ
+ # インスタンス変数を使う場合
+before do
+  @user = User.new(name: 'Taro', email: 'taro@example.com')
+end
+
+it 'is valid' do
+  expect(@user).to be_valid
+end
+
+# letを使う場合
+let(:user) { User.new(name: 'Taro', email: 'taro@example.com') }
+
+it 'is valid' do
+  expect(user).to be_valid
+end
+
+
+
+
+ #def edit
+  #  @main_content = @prototype.prototype_images.first
+  #  @sub_contents = @prototype.prototype_images.sub
+ # end
+=end
+
+
+
+
+
