d734bc3027be40ac5a99a34e4f2fd7e5ba1dcd99
ShizukaHara <xsweet926x@gmail.com>
2017-05-02 18:06:42 +0900

    delete many comments_out

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -18,20 +18,16 @@ describe PrototypesController, type: :controller do
         .to receive(:current_user)
         .and_return(@user)
     end
-    #current_userのスタブを作る。before_action require_loginを通過できる。Qita参照。
+    #current_userのスタブを作る。before_action require_loginを通過できる。
 
 
     describe 'GET #index' do
       it 'assigns the requested prototypes to @prototypes' do
-        binding.pry
         prototypes = create_list(:prototype, 8)
         get :index
         expect(assigns(:prototypes)).to eq (prototypes.sort{ |a,b| b.created_at <=> a.created_at })
       end
 
-    #@prototypes = Prototype.includes(:user, :thumbnails, :tags).order('created_at DESC').page(params[:page]).per(8)
-    #indexアクションエラー未解決
-    #そしてMySQLにログインできない悲劇。
       it "renders the :index template" do
         prototype = create(:prototype)
         get :index
@@ -104,41 +100,4 @@ describe PrototypesController, type: :controller do
 
 
 end
-
-
-
-
-
-
-
-=begin
-メモ
- # インスタンス変数を使う場合
-before do
-  @user = User.new(name: 'Taro', email: 'taro@example.com')
-end
-
-it 'is valid' do
-  expect(@user).to be_valid
-end
-
-# letを使う場合
-let(:user) { User.new(name: 'Taro', email: 'taro@example.com') }
-
-it 'is valid' do
-  expect(user).to be_valid
-end
-
-
-
-
- #def edit
-  #  @main_content = @prototype.prototype_images.first
-  #  @sub_contents = @prototype.prototype_images.sub
- # end
-=end
-
-
-
-
 
